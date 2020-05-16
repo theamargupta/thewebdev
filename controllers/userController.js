d91ceaed8bf182ddfd68c63a2574d7392d8213ad
Amar Gupta <amar9911056196@gmail.com>
2020-05-16 11:43:13 +0530

    TheWebDev

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User')
+
+exports.login = () => {
+    
+}
+
+exports.logout = () => {
+    
+}
+
+exports.register = function (req, res)  {
+    let user= new User(req.body)
+    user.register
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send('congrates')
+    }
+}
+exports.home = (req, res) => {
+    res.render('home-guest')
+}
