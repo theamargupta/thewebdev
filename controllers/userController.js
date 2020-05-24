4c84b1e5bba2e6957bff76e4b83375d756759ed3
Amar Gupta <amar9911056196@gmail.com>
2020-05-24 14:12:05 +0530

    UPDATE

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,3 +65,19 @@ exports.home = (req, res) => {
         res.render('home-guest', {err: req.flash('err'), regErr: req.flash('regErr')})
     }
 }
+
+exports.ifUserExists = function(req, res, next) {
+    User.findByUsername(req.params.username).then(function(userDocument) {
+      req.profileUser = userDocument
+      next()
+    }).catch(function() {
+      res.render("404")
+    })
+  }
+  
+  exports.profilePostsScreen = function(req, res) {
+    res.render('profile', {
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar
+    })
+  }
--- router.js
+++ router.js
@@ -9,6 +9,9 @@ router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+// Profile ralted route
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
 // post related routes
 router.get('/create-post', userController.mustLog, postController.viewCreateScreen)
 router.post('/create-post', userController.mustLog, postController.create )
